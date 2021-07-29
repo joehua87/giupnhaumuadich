9c8132512cf764893cba64035027919a8253d080
Joe Hua <joehua87@gmail.com>
2021-07-29 16:22:27 +0700

    correct config

--- config/releases.exs
+++ config/releases.exs
@@ -1,11 +1,11 @@
 import Config
 
-config :yum, Giupnhaumuadich.Repo,
+config :giupnhaumuadich, Giupnhaumuadich.Repo,
   # ssl: true,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-config :yum_web, GiupnhaumuadichWeb.Endpoint,
+config :giupnhaumuadich_web, GiupnhaumuadichWeb.Endpoint,
   http: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
